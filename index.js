68c29b64eab0149b8fc21b435d64f563bbd593bd
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-18 17:01:47 -0300

    used getter method to DRY up logic

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ class Transaction {
     this.account = account;
   }
 
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value () {
+    return this.amount
   }
 }
+
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 }
-
 
 class Account {
 
@@ -40,16 +43,31 @@ class Account {
 
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25, myAccount);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', myAccount.balance);
+console.log('Ending Balance:', myAccount.balance);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
+
+
+
+
+
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// console.log('Balance:', myAccount.balance);
+
+// t3 = new Deposit(120.00, myAccount);
+// t3.commit();
+// console.log('Transaction 3:', t3);
